41a1ef54b38c29c5fa608e43fdb31d6a34485788
Andrew SungWoo Chun <andrewchun041@gmail.com>
2024-07-20 07:13:14 -0700

    add scroll event listener; add variables that store dynamic image pixel position; add conditional statements for when images are at certain positions; add class active to the elements if conditions are met for slide effect

--- script.js
+++ script.js
@@ -12,3 +12,26 @@ function debounce(func, wait = 20, immediate = true) {
     if (callNow) func.apply(context, args);
     };
 }
+
+const sliderImages = document.querySelectorAll('.slide-in');
+
+function checkSlide() {
+    sliderImages.forEach(sliderImage => {
+        // pixel position at the bottom of screen - half of each image height
+        // half way through image
+        const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+        // pixel position at the bottom of each image
+        // bottom of image
+        const imageBottom = sliderImage.offsetTop + sliderImage.height;
+
+        const isHalfShown = slideInAt > sliderImage.offsetTop;
+        const isNotScrolledPast = window.scrollY < imageBottom;
+        if (isHalfShown && isNotScrolledPast) {
+            sliderImage.classList.add('active');
+        } else {
+            sliderImage.classList.remove('active');
+        }
+    });
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
